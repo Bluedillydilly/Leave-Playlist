68b4599ac07f94ab3e1c698b0520c43f9a168506
blue <dylancuprewich@gmail.com>
2020-04-28 18:42:06 -0400

    init commit

---
+++ leave-playlist.js
@@ -0,0 +1,11 @@
+// add a button
+// upon change page url to remove reference to a playlist
+
+
+document.body.style.border = "5px solid red";
+
+//let btn = document.createElement("button");
+//btn.innerHTML = "TEST TEST TEST";
+//document.appendChild(btn);
+//window.location.replace("https://google.com")
+//log("HELLO HELLO HELLO")
